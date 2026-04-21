a33343ec545acdbab677103d80061f9e123dc6ff
kapilshah <kapilshh3@gmail.com>
2026-04-22 00:55:42 +0530

    final working type

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request  #framework...
+from fastapi import FastAPI, Request
 #this is the import used for redirecting the user's url to another shorter url 
 from fastapi.responses import RedirectResponse 
 
@@ -11,6 +11,7 @@ import json
 import os
 
 from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
@@ -23,7 +24,6 @@ def load_data():
         try:
             with open(DB_FILE, "r") as f:
                 return json.load(f) #conversion into dictionrry 
-            #file handling 
         except:
             return {}     
     return {}
@@ -34,12 +34,13 @@ def save_data(data):
 
 url_db = load_data()
 
-def generate_id(length=6):
+def generate_id(length=4):
     characters = string.ascii_letters + string.digits
     return ''.join(random.choice(characters) for _ in range(length))
 
 class URLRequest(BaseModel):
     long_url: str
+    custom_id: Optional[str] = None
 
 @app.get("/")
 def home():
@@ -47,13 +48,34 @@ def home():
 
 @app.post("/shorten")
 def shorten_url(data: URLRequest, request: Request):
-    short_id = generate_id()
-    url_db[short_id] = data.long_url
+    
+    #use custom id if provided else generate
+    short_id = data.custom_id if data.custom_id else generate_id()
+
+    #check if already exists
+    if short_id in url_db:
+        return {"error": "Short ID already exists, try another"}
+
+    #store url + clicks
+    url_db[short_id] = {
+        "url": data.long_url,
+        "clicks": 0
+    }
+
     save_data(url_db)
+
     return {"short_url": f"{request.base_url}{short_id}"}
 
 @app.get("/{short_id}")
 def redirect_url(short_id: str):
     if short_id in url_db:
-        return RedirectResponse(url_db[short_id])
+        url_db[short_id]["clicks"] += 1
+        save_data(url_db)
+        return RedirectResponse(url_db[short_id]["url"])
     return {"error": "URL not found"}
+
+@app.get("/stats/{short_id}")
+def get_stats(short_id: str):
+    if short_id in url_db:
+        return url_db[short_id]
+    return {"error": "Not found"}
